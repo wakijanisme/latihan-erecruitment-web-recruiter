8d035fff9eecc1f79eb483b922dc3e1e004a0e43
wakijanisme <ahmadsyaughi@gmail.com>
2024-05-16 14:14:09 +0700

    inquiry master pre screening

--- features/scenarios/login.feature
+++ features/scenarios/login.feature
@@ -1,5 +1,5 @@
-@login
-Feature: login page
+@web-recruiter
+Feature: login page and setup
 
   @test-login
   Scenario: success login with correct credentials
@@ -7,3 +7,12 @@ Feature: login page
     When I click sign in on the homepage
     And I fill my credentials on the homepage
     Then I should be logged in
+
+  @master-pre-screening
+  Scenario: login sampai pembuatan setup
+    Given I am on the homepage web recruiter
+    When I click sign in on the homepage
+    And I fill my credentials on the homepage
+    And Click Job Vacancy Management
+    And Click Master Pre Screening
+    Then Show Pre-Screening Question Master Inquiry
--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -12,6 +12,22 @@ When('I fill my credentials on the homepage') do
   find(:xpath, "//span[@class='btn-caption' and contains(text(),'Log In')]").click
 end
 
+When('Click Job Vacancy Management') do
+  find(:xpath, "//span[@class='anchor-caption' and contains(text(),'Job Vacancy Management')]").click
+  sleep 2
+end
+
+When('Click Master Pre Screening') do
+  find(:xpath, "//span[@class='anchor-caption' and contains(text(),'Master Pre-Screening')]").click
+  sleep 2
+end
+
 Then('I should be logged in') do
   find(:xpath, "//label[@name='label1' and contains(text(),'Home')]").should be_visible
+  sleep 2
+end
+
+Then('Show Pre-Screening Question Master Inquiry') do
+  find(:xpath, "//label[@name='label1' and contains(text(),'Pre-Screening Question Master')]").should be_visible
+  sleep 2
 end
